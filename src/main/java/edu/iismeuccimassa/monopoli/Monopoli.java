b79e14c32d65143bfffcc7a4b8e6a04988ecd482
liucscai@tiscalinet.it <liucscai@tiscalinet.it>
2026-03-14 17:29:20 +0100

    creato progetto NetBeans

---
+++ src/main/java/edu/iismeuccimassa/monopoli/Monopoli.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.iismeuccimassa.monopoli;
+
+/**
+ *
+ * @author luca
+ */
+public class Monopoli {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
