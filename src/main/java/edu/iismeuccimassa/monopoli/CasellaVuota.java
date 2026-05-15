c0a7d322150618f7baa48109cca7f2bb4b8f186e
AleMarza08 <alessandro.marzano.ake@gmail.com>
2026-05-15 10:35:46 +0200

    Modify CasellaVuota to manage player prison action
    Update CasellaVuota class to handle player actions.

--- src/main/java/edu/iismeuccimassa/monopoli/CasellaVuota.java
+++ src/main/java/edu/iismeuccimassa/monopoli/CasellaVuota.java
@@ -1,12 +1,13 @@
 package edu.iismeuccimassa.monopoli;
 
 import javax.swing.*;
-
+//classe CasellaVuota che eredita da Casella
 public class CasellaVuota extends Casella {
     CasellaVuota(String nome, TipoCasella tipo) {
         super(nome, tipo, null);
     }
-
+    //le CaselleVuote sono le caselle che attualmente non fanno niente ad attivazione di effetto
+    //questo metodo attualmente controlla che se il giocatore è finito sulla casella vai in prigione lo posiziona lì
     @Override
     void eseguiAzione(Giocatore g) {
         if (tipo == TipoCasella.VAI_IN_PRIGIONE) {
